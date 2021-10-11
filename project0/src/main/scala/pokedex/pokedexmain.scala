65f4aa41c6a15a4554723cc63f308a9d8d5e81bd
Nathanael Mottonen <mottonen12n@gmail.com>
2021-10-11 15:53:50 -0400

    Added UI skeleton

--- project0/src/main/scala/pokedex/pokedexmain.scala
+++ project0/src/main/scala/pokedex/pokedexmain.scala
@@ -7,7 +7,7 @@ import scala.io.BufferedSource
 object Pokedex  {
     def main(args: Array[String]){
 
-        val fileName = "C:/Users/Work/Project0/pokedex2.csv"
+        val fileName = "C:/Users/Work/Project0/pokedex.csv"
         var bufferedSource:BufferedSource = null
 
         try{
@@ -15,7 +15,7 @@ object Pokedex  {
             for (line <- bufferedSource.getLines) {
                 val cols = line.split(",").map(_.trim)
                 //Do processing of input here
-                println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}|${cols(4)}|${cols(5)}")
+                println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}|${cols(4)}|${cols(5)}|${cols(6)}")
                 //println(cols(0))
                 //println(cols(1))
                 //println(cols(2))
@@ -37,5 +37,70 @@ object Pokedex  {
                 case e: NullPointerException => None
             }
         }
+
+        var loop = true
+        do {
+            var selection = -1
+            var nameToSearch = ""
+            var numberToSearch = 0
+            var typeToSearch = ""
+            var pokemonToCatch = ""
+            loop = true
+
+            println("Welcome to the Pokedex! Please choose an operation:\n" +
+              "(1) Search by Name\n" +
+              "(2) Search by Number\n" +
+              "(3) Search by Type\n" +
+              "(4) Set a Pokemon to Caught\n" +
+              "(0) Exit Pokedex")
+            try {
+                selection = readInt()
+                selection match {
+                case 1 => {
+                    println("Enter Pokemon name to search: ")
+                    nameToSearch = readLine().trim()
+                    println(nameToSearch) //Processing goes here
+                }
+                case 2 => {
+                    println("Enter Pokemon number to search: ")
+                    numberToSearch = readInt()
+                    println(numberToSearch) //Processing goes here
+                }
+                case 3 => {
+                    println("Enter Pokemon type to search: ")
+                    typeToSearch = readLine().trim()
+                    if(checkType(typeToSearch)){
+                        println(typeToSearch) //Processing goes here
+                    } else println(s"Invalid type entered: $typeToSearch")
+                }
+                case 4 => {
+                    println("Enter the Pokemon that you caught: ")
+                    pokemonToCatch = readLine().trim()
+                    println(pokemonToCatch) //Processing goes here
+                }
+                case 0 => {
+                    println("Thank you for using the Pokedex!")
+                    Thread.sleep(1000)
+                    loop = false
+                }
+                case _ => {
+                    println("Invalid selection, please try again.")
+                }
+            }
+            }
+            catch {
+                case e: Exception => println("Invalid selection, please try again.")
+            }
+            
+            }
+        while(loop)
+
+        def checkType(givenType: String) : Boolean = {
+            givenType.trim.toUpperCase match {
+                case "NORMAL" | "FIRE" | "WATER" | "GRASS" | "ELECTRIC" | "ICE" | "FIGHTING" | "POISON" | "GROUND" | "FLYING" | "PSYCHIC" | "BUG" | "ROCK" | "GHOST" | "DARK" | "DRAGON" | "STEEL" | "FAIRY" => return true
+                case _ => return false
+            }
+        }
+
     }
 }
